7a4f6b79828d9b569adc1b24a962c31a0c8f9c97
moritzgoeckel <moritz.goeckel@gmail.com>
2018-08-27 18:14:17 +0200

    Changed the install to apply

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,15 @@
-kubectl create -f agt-article-redis.yml
-kubectl create -f agt-link-redis.yml
+kubectl apply -f agt-article-redis.yml
+kubectl apply -f agt-link-redis.yml
 
-kubectl create -f pq-redis.yml
-kubectl create -f lq-redis.yml
+kubectl apply -f pq-redis.yml
+kubectl apply -f lq-redis.yml
 
-kubectl create -f cache-redis.yml
-kubectl create -f mongodb.yml
-kubectl create -f elastic.yml
+kubectl apply -f cache-redis.yml
+kubectl apply -f mongodb.yml
+kubectl apply -f elastic.yml
 
-kubectl create -f article-downloader.yml
-kubectl create -f link-downloader.yml
-kubectl create -f processor.yml
-kubectl create -f static-content-server.yml
-kubectl create -f api-server.yml
+kubectl apply -f article-downloader.yml
+kubectl apply -f link-downloader.yml
+kubectl apply -f processor.yml
+kubectl apply -f static-content-server.yml
+kubectl apply -f api-server.yml
