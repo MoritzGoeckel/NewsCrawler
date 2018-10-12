47688e5b6bf5660bfb5b883539be592ff0572241
Joapfel <kraemer_johannes@gmx.de>
2018-10-12 18:01:06 +0200

    added time utils

---
+++ api-server-image/content/timeUtils.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"time"
+
+	"github.com/vjeantet/jodaTime"
+)
+
+const year = "YYYY"
+const yearMonth = "YYYY.MM"
+const yearMonthDay = "YYYY.MM.dd"
+const yearMonthDayHour = "YYYY.MM.dd:HH"
+const yearMonthDayHourMinute = "YYYY.MM.dd:HH.mm"
+const yearMonthDayHourMinuteSecond = "YYYY.MM.dd:HH.mm.ss"
+
+//time getters
+func getYear() string {
+	return jodaTime.Format(year, time.Now())
+}
+func getMonth() string {
+	return jodaTime.Format(yearMonth, time.Now())
+}
+func getToday() string {
+	return jodaTime.Format(yearMonthDay, time.Now())
+}
+func getHour() string {
+	return jodaTime.Format(yearMonthDayHour, time.Now())
+}
+func getMinute() string {
+	return jodaTime.Format(yearMonthDayHourMinute, time.Now())
+}
+func getSecond() string {
+	return jodaTime.Format(yearMonthDayHourMinuteSecond, time.Now())
+}
+
+//time past/future getters for year,month,day
+func getTimeXYearsAgo(x int, format string) string {
+	return jodaTime.Format(format, time.Now().AddDate(-x, 0, 0))
+}
+func getTimeXMonthsAgo(x int, format string) string {
+	return jodaTime.Format(format, time.Now().AddDate(0, -x, 0))
+}
+func getTimeXDaysAgo(x int, format string) string {
+	return jodaTime.Format(format, time.Now().AddDate(0, 0, -x))
+}
+func getTimeInXYears(x int, format string) string {
+	return jodaTime.Format(format, time.Now().AddDate(x, 0, 0))
+}
+func getTimeInXMonths(x int, format string) string {
+	return jodaTime.Format(format, time.Now().AddDate(0, x, 0))
+}
+func getTimeInXDays(x int, format string) string {
+	return jodaTime.Format(format, time.Now().AddDate(0, 0, x))
+}
+
+//time past/future getters for hour,minute,second
+func getTimeXHoursAgo(x int, format string) string {
+	return jodaTime.Format(format, time.Now().Add(-x*time.Hour))
+}
+func getTimeXMinAgo(x int, format string) string {
+	return jodaTime.Format(format, time.Now().Add(-x*time.Minute))
+}
+func getTimeXSecondsAgo(x int, format string) string {
+	return jodaTime.Format(format, time.Now().Add(-x*time.Second))
+}
+func getTimeInXHours(x int, format string) string {
+	return jodaTime.Format(format, time.Now().Add(x*time.Hour))
+}
+func getTimeInXMin(x int, format string) string {
+	return jodaTime.Format(format, time.Now().Add(x*time.Minute))
+}
+func getTimeInXSeconds(x int, format string) string {
+	return jodaTime.Format(format, time.Now().Add(x*time.Second))
+}
